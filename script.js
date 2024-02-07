2c2b9e91f89563148f1a5e1c7536b201af7c475b
polina <87384505+imfrompluto@users.noreply.github.com>
2024-02-07 17:41:54 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -8,11 +8,28 @@ let botsteps = 0
 let diceroll = 1
 let bot = document.getElementById("bot")
 let victory = document.getElementById("victory")
+let diceinterval
+let currentinterval
+function again(){
+    steps = 0
+    botsteps = 0
+    victory.style.opacity = 0
+    bot.style.left = 0
+    character.style.left = 0
+    bot.style.bottom = 0
+    character.style.bottom = 0
+    dice.style.pointerEvents = "auto"
+    clearInterval(diceinterval)
+    clearInterval(currentinterval)
+    botplaying = false
+    document.getElementById("player").innerHTML = "player: human"
+}
+// Array
 let maps = [
     "snakes n ladders2.png",
     "snakes0.png",
 ]
-map.src = maps[Math.floor(Math.random()*2)]
+map.src = maps[Math.floor(Math.random() * 2)]
 function movecharacter() {
     console.log(steps);
     if (!botplaying) {
@@ -49,7 +66,7 @@ function movecharacter() {
         }
     }
 }
-
+// function for the character to move
 movecharacter()
 
 dice.onclick = function () {
@@ -61,7 +78,7 @@ dice.onclick = function () {
 
 
 
-    let diceinterval = setInterval(() => {
+        diceinterval = setInterval(() => {
         console.log("dice");
         diceroll = Math.floor(Math.random() * 6 + 1)
 
@@ -70,179 +87,212 @@ dice.onclick = function () {
 
     }, 100);
     setTimeout(() => {
+        // stops the repeated execution
         clearInterval(diceinterval)
-        dice.style.pointerEvents = "auto"
         console.log(map.src)
         if (!botplaying) {
             let currentsteps = 0
-           let currentinterval = setInterval(() => {
-               console.log(steps);
-               if (currentsteps == diceroll){
-                   botplaying = !botplaying
-                   if (botplaying) {
-                       
-                       // change the html
-                       document.getElementById("player").innerHTML = "player: bot"
-                       
-                    }
-                    else {
-                        document.getElementById("player").innerHTML = "player: human"
-                    }
+            currentinterval = setInterval(() => {
+                console.log(steps);
+                if (currentsteps == diceroll) {
                     clearInterval(currentinterval)
+                    dice.style.pointerEvents = "auto"
+                    if (map.src.includes("snakes0")) {
+                        if (steps == 5) {
+                            steps = 26
+                        }
+                        else if (steps == 8) {
+                            steps = 49
+                        }
+                        else if (steps == 19) {
+                            steps = 38
+                        }
+                        else if (steps == 24) {
+                            steps = 56
+                        }
+                        else if (steps == 52) {
+                            steps = 71
+                        }
+                        else if (steps == 53) {
+                            steps = 84
+                        }
+                        else if (steps == 60) {
+                            steps = 81
+                        }
+                        else if (steps == 42) {
+                            steps = 15
+                        }
+                        else if (steps == 54) {
+                            steps = 33
+                        }
+                        else if (steps == 77) {
+                            steps = 41
+                        }
+                        else if (steps == 69) {
+                            steps = 47
+                        }
+                        else if (steps == 94) {
+                            steps = 72
+                        }
+                        else if (steps == 95) {
+                            steps = 81
+                        }
+                        movecharacter()
+                        botplaying = true
+                    }
+                    else if (map.src.includes("ladders")) {
+                        console.log("snakes and ladders")
+                        if (steps == 16) {
+                            steps = 41
+                        }
+                        else if (steps == 32) {
+                            steps = 52
+                        }
+                        else if (steps == 56) {
+                            steps = 95
+                        }
+                        else if (steps == 33) {
+                            steps = 12
+                        }
+                        else if (steps == 44) {
+                            steps = 37
+                        }
+                        else if (steps == 72) {
+                            steps = 49
+                        }
+                        else if (steps == 93) {
+                            steps = 62
+                        }
+                        movecharacter()
+                        botplaying = true
+                    }
+                   
                 }
-                else{
+                else {
                     steps = steps + 1
+                    if (steps >= 99) {
+                        steps = 99
+                        victory.style.opacity = 1
+                        dice.style.pointerEvents = "none"
+                        victory.innerHTML = "victory!"
+                    }
+                    movecharacter()
                 }
-                currentsteps = currentsteps + 1 
-                movecharacter()
-            },800);
-            // steps = steps + diceroll
-            // if (map.src.includes("snakes0")) {
-            //     if (steps == 5) {
-            //         steps = 26
-            //     }
-            //     else if (steps == 8) {
-            //         steps = 49
-            //     }
-            //     else if (steps == 19) {
-            //         steps = 38
-            //     }
-            //     else if (steps == 24) {
-            //         steps = 56
-            //     }
-            //     else if (steps == 52) {
-            //         steps = 71
-            //     }
-            //     else if (steps == 53) {
-            //         steps = 84
-            //     }
-            //     else if (steps == 60) {
-            //         steps = 81
-            //     }
-            //     else if (steps == 42) {
-            //         steps = 15
-            //     }
-            //     else if (steps == 54) {
-            //         steps = 33
-            //     }
-            //     else if (steps == 77) {
-            //         steps = 41
-            //     }
-            //     else if (steps == 69) {
-            //         steps = 47
-            //     }
-            //     else if (steps == 94) {
-            //         steps = 72
-            //     }
-            //     else if (steps == 95) {
-            //         steps = 81
-            //     }
-            // }
-            // else if (map.src.includes("ladders")) {
-            //     console.log("snakes and ladders")
-            //     if (steps == 16) {
-            //         steps = 41
-            //     }
-            //     else if (steps == 32) {
-            //         steps = 52
-            //     }
-            //     else if (steps == 56) {
-            //         steps = 95
-            //     }
-            //     else if (steps == 33) {
-            //         steps = 12
-            //     }
-            //     else if (steps == 44) {
-            //         steps = 37
-            //     }
-            //     else if (steps == 72) {
-            //         steps = 49
-            //     }
-            //     else if (steps == 93) {
-            //         steps = 62
-            //     }
-            // }
-            // if (steps > 99) {
-            //     steps = 99
-            // }
+                currentsteps = currentsteps + 1
+                if (botplaying) {
+
+                    // change the html
+                    document.getElementById("player").innerHTML = "player: bot"
+
+                }
+                else {
+                    document.getElementById("player").innerHTML = "player: human"
+                }
+
+            }, 800);
+
         }
         else {
-            botplaying = false
+            let currentsteps = 0
+            let currentinterval = setInterval(() => {
+                console.log(steps);
+                if (currentsteps == diceroll) {
+                    clearInterval(currentinterval)
+                    dice.style.pointerEvents = "auto"
+                    if (map.src.includes("snakes0")) {
+                        if (botsteps == 5) {
+                            botsteps = 26
+                        }
+                        else if (botsteps == 8) {
+                            botsteps = 49
+                        }
+                        else if (botsteps == 19) {
+                            botsteps = 38
+                        }
+                        else if (botsteps == 24) {
+                            botsteps = 56
+                        }
+                        else if (botsteps == 52) {
+                            botsteps = 71
+                        }
+                        else if (botsteps == 53) {
+                            botsteps = 84
+                        }
+                        else if (botsteps == 60) {
+                            botsteps = 81
+                        }
+                        else if (botsteps == 42) {
+                            botsteps = 15
+                        }
+                        else if (botsteps == 54) {
+                            botsteps = 33
+                        }
+                        else if (botsteps == 77) {
+                            botsteps = 41
+                        }
+                        else if (botsteps == 69) {
+                            botsteps = 47
+                        }
+                        else if (botsteps == 94) {
+                            botsteps = 72
+                        }
+                        else if (botsteps == 95) {
+                            botsteps = 81
+                        }
+                        movecharacter()
+                        botplaying = false
+                    }
+                    else if (map.src.includes("ladders")) {
+                        console.log("snakes and ladders")
+                        if (botsteps == 16) {
+                            botsteps = 41
+                        }
+                        else if (botsteps == 32) {
+                            botsteps = 52
+                        }
+                        else if (botsteps == 56) {
+                            botsteps = 95
+                        }
+                        else if (botsteps == 33) {
+                            botsteps = 12
+                        }
+                        else if (botsteps == 44) {
+                            botsteps = 37
+                        }
+                        else if (botsteps == 72) {
+                            botsteps = 49
+                        }
+                        else if (botsteps == 93) {
+                            botsteps = 62
+                        }
+                        movecharacter()
+                        botplaying = false
+                    }
+                 
+                }
+                else {
+                    botsteps = botsteps + 1
+                    if (botsteps > 99) {
+                        botsteps = 99
+                        victory.style.opacity = 1
+                        document.getElementById("victory").innerHTML = "you lost!"
+                        dice.style.pointerEvents = "none"
+                    }
+                    movecharacter()
+                }
+                currentsteps = currentsteps + 1
+                if (botplaying) {
 
-            // botsteps = botsteps + diceroll
-            // if (map.src.includes("snakes0")) {
-            //     if (botsteps == 5) {
-            //         botsteps = 26
-            //     }
-            //     else if (botsteps == 8) {
-            //         botsteps = 49
-            //     }
-            //     else if (botsteps == 19) {
-            //         botsteps = 38
-            //     }
-            //     else if (botsteps == 24) {
-            //         botsteps = 56
-            //     }
-            //     else if (botsteps == 52) {
-            //         botsteps = 71
-            //     }
-            //     else if (botsteps == 53) {
-            //         botsteps = 84
-            //     }
-            //     else if (botsteps == 60) {
-            //         botsteps = 81
-            //     }
-            //     else if (botsteps == 42) {
-            //         botsteps = 15
-            //     }
-            //     else if (botsteps == 54) {
-            //         botsteps = 33
-            //     }
-            //     else if (botsteps == 77) {
-            //         botsteps = 41
-            //     }
-            //     else if (botsteps == 69) {
-            //         botsteps = 47
-            //     }
-            //     else if (botsteps == 94) {
-            //         botsteps = 72
-            //     }
-            //     else if (botsteps == 95) {
-            //         botsteps = 81
-            //     }
-            // }
-            // else if (map.src.includes("ladders")) {
-            //     console.log("snakes and ladders")
-            //     if (botsteps == 16) {
-            //         botsteps = 41
-            //     }
-            //     else if (botsteps == 32) {
-            //         botsteps = 52
-            //     }
-            //     else if (botsteps == 56) {
-            //         botsteps = 95
-            //     }
-            //     else if (botsteps == 33) {
-            //         botsteps = 12
-            //     }
-            //     else if (botsteps == 44) {
-            //         botsteps = 37
-            //     }
-            //     else if (botsteps == 72) {
-            //         botsteps = 49
-            //     }
-            //     else if (botsteps == 93) {
-            //         botsteps = 62
-            //     }
-            // }
-            // if (botsteps > 99) {
-            //     botsteps = 99
-            //     victory.style.opacity = 1
-            // }
+                    // change the html
+                    document.getElementById("player").innerHTML = "player: bot"
+
+                }
+                else {
+                    document.getElementById("player").innerHTML = "player: human"
+                }
+
+            }, 800);
         }
-
-        // movecharacter()
-       
     }, 2000)
 }
 
